fc364217c48c7afb43d9e9decb0f1a866959d234
Jmann777 <87671742+Jmann777@users.noreply.github.com>
2024-06-03 16:51:01 +0100

    Updates PC

--- Source/model.py
+++ Source/model.py
@@ -1,6 +1,10 @@
 
+
+# Drop penalties
+
 # Scatter the goals
 
+# Compare my xg to statsbomb
 
 https://www.youtube.com/watch?v=zM4VZR0px8E
 https://stackoverflow.com/questions/50733014/linear-regression-with-dummy-categorical-variables
